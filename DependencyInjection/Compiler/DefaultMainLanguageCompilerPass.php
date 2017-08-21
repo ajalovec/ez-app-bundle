80489237133b6026e7a1f8236ada01e95bd1fd89
Andraz Jalovec <andraz.jalovec@gmail.com>
2017-08-21 20:31:44 +0200

    Compiler for resolving default main language

---
+++ DependencyInjection/Compiler/DefaultMainLanguageCompilerPass.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Origammi\Bundle\EzAppBundle\DependencyInjection\Compiler;
+
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+/**
+ * Class DefaultMainLanguageCompilerPass
+ *
+ * @package   Origammi\Bundle\EzAppBundle\DependencyInjection\Compiler
+ * @author    Andraž Jalovec <andraz.jalovec@origammi.co>
+ * @copyright 2017 Origammi (http://origammi.co)
+ */
+class DefaultMainLanguageCompilerPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container)
+    {
+        $defaultSiteaccess = $container->getParameterBag()->get('ezpublish.siteaccess.default');
+        $languages         = $container->getParameterBag()->get('ezsettings.' . $defaultSiteaccess . '.languages');
+
+        $container->setParameter('origammi_ezapp.main_language_code', array_shift($languages));
+    }
+
+}
