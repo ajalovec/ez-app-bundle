8addc8d3e1e4f4aa00e47870e74db80b32a21e72
Andraz Jalovec <andraz.jalovec@gmail.com>
2017-08-21 20:33:23 +0200

    Register compiler pass and services

--- OrigammiEzAppBundle.php
+++ OrigammiEzAppBundle.php
@@ -5,8 +5,16 @@
 
 namespace Origammi\Bundle\EzAppBundle;
 
+use Origammi\Bundle\EzAppBundle\DependencyInjection\Compiler\DefaultMainLanguageCompilerPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class OrigammiEzAppBundle extends Bundle
 {
+    public function build(ContainerBuilder $builder)
+    {
+        parent::build($builder);
+
+        $builder->addCompilerPass(new DefaultMainLanguageCompilerPass());
+    }
 }
