6468a007757e8cbcea8e49752f1db349dae522f1
Andraz Jalovec <andraz.jalovec@gmail.com>
2018-02-20 10:56:56 +0100

    Create User command

--- Command/User/CreateUserCommand.php
+++ Command/User/CreateUserCommand.php
@@ -7,8 +7,8 @@ namespace Origammi\Bundle\EzAppBundle\Command\User;
 
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Question\ChoiceQuestion;
 use Symfony\Component\Console\Question\Question;
 
 /**
@@ -34,16 +34,34 @@ class CreateUserCommand extends BaseCommand
             ->addArgument('username', InputArgument::REQUIRED, 'The username')
             ->addArgument('email', InputArgument::REQUIRED, 'The email')
             ->addArgument('password', InputArgument::REQUIRED, 'The password')
-            ->addArgument('groups', InputArgument::IS_ARRAY | InputArgument::REQUIRED, 'Add user to a group.')
+            ->addArgument('groups', InputArgument::IS_ARRAY | InputArgument::REQUIRED, 'One or more user group ids (separated with `,) <comment>[example: 22,44,24,63]</comment>')
+            ->addOption('disabled', null, InputOption::VALUE_NONE, 'Disable the user by default')
+            ->addOption('admin', null, InputOption::VALUE_NONE, 'Set the user as admin')
             ->setHelp(<<<'EOT'
-The <info>fos:user:create</info> command creates a user:
+The <info>%command.name%</info> command creates new user:
+
   <info>php %command.full_name% acme</info>
-This interactive shell will ask you for an email and then a password.
-You can alternatively specify the email and password as the second and third arguments:
-  <info>php %command.full_name% acme acme@email.com mypassword</info>
-You can assign user to one or more groups (separate ids with ,):
-  <info>php %command.full_name% acme acme@email.com mypassword 3</info>
-  <info>php %command.full_name% acme acme@email.com mypassword 5,33,44,55,66</info>
+  
+This interactive shell will ask you for more required arguments.
+
+You can alternatively specify more arguments when writing the command name:
+
+  <info>%command.full_name% acme acme@email.com</info>
+  
+  <info>%command.full_name% acme acme@email.com 1234</info>
+  
+You have to specify one or more user groups or use --admin option to assign user to administrator group:
+
+  <info>%command.full_name% acme acme@email.com 1234 3</info>
+  
+  <info>%command.full_name% acme acme@email.com 1234 5,33,44,55,66</info>
+  
+  <info>%command.full_name% acme acme@email.com 1234 --admin</info>
+
+You can create disabled user (will not be able to log in):
+
+  <info>%command.full_name% acme acme@email.com 1234 --disabled</info>
+
 EOT
             )
         ;
@@ -67,9 +85,11 @@ EOT
         $user['password']   = $input->getArgument('password');
         $user['first_name'] = $user['last_name'] = $user['username'];
 
-        $this->getUserManager()->create($user, $userGroups, 'eng-GB');
+        return;
+        $this->getUserManager()->create($user, $userGroups);
 
         $output->writeln(sprintf('Created user <comment>%s</comment>', $user['username']));
+
         return;
     }
 
@@ -99,71 +119,14 @@ EOT
             $questions['password'] = $question;
         }
 
-        if (!$input->getArgument('groups')) {
-            $api = $this->getContainer()->get('origammi_ezapp.api');
-
-            $usersRootLocation = $api->loadLocation(5);
-            $userGroups        = $api->getLocationService()->findBySubtree($usersRootLocation, ['user_group']);
-            $groups            = [];
-
-            foreach ($userGroups as $userGroup) {
-                $groups[$userGroup->contentId] = $userGroup->contentInfo->name;
-            }
-
-            $question = new ChoiceQuestion('Please choose user groups:', $groups);
-            $question
-                ->setMultiselect(true)
-                ->setValidator($this->getChoicesValidator($groups))
-            ;
-            $questions['groups'] = $question;
-        }
+//        if (!$input->getArgument('groups')) {
+//            $questions['groups'] = $this->createUserGroupsChoiceQuestion();
+//        }
 
         foreach ($questions as $name => $question) {
             $answer = $this->getHelper('question')->ask($input, $output, $question);
 
             $input->setArgument($name, $answer);
         }
-    }
-
-    /**
-     * @param string $errorMessage
-     *
-     * @return \Closure
-     */
-    private function getIsEmptyValidator($errorMessage = 'Value can not be empty')
-    {
-        return function ($value) use ($errorMessage) {
-            if (empty($value)) {
-                throw new \Exception($errorMessage);
-            }
-
-            return $value;
-        };
-    }
-
-    /**
-     * @param string $errorMessage
-     *
-     * @return \Closure
-     */
-    private function getChoicesValidator(array $choices, $errorMessage = 'Value can not be empty')
-    {
-        return function ($selected) use ($choices, $errorMessage) {
-            if (!preg_match('/^[^,]+(?:,[^,]+)*$/', $selected, $matches)) {
-                throw new \InvalidArgumentException(sprintf($errorMessage, $selected));
-            }
-            $selectedChoices = explode(',', $selected);
-
-            $multiselectChoices = array();
-            foreach ($selectedChoices as $value) {
-                if (!isset($choices[$value])) {
-                    throw new \InvalidArgumentException(sprintf('The provided answer is ambiguous. Value should be one of %s.', implode(' or ', array_keys($choices))));
-                }
-
-                $multiselectChoices[] = $value;
-            }
-
-            return $multiselectChoices;
-        };
     }
 }
