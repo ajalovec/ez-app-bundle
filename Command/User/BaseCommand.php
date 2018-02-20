507600e1711e7e35d98941c60ba2882fe2641ae1
Andraz Jalovec <andraz.jalovec@gmail.com>
2018-02-20 10:55:23 +0100

    Base User command

--- Command/User/BaseCommand.php
+++ Command/User/BaseCommand.php
@@ -6,9 +6,10 @@
 namespace Origammi\Bundle\EzAppBundle\Command\User;
 
 use eZ\Publish\API\Repository\UserService;
-use eZ\Publish\Core\MVC\ConfigResolverInterface;
 use Origammi\Bundle\EzAppBundle\Manager\UserManager;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * Class BaseCommand
@@ -19,20 +20,50 @@ use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
  */
 abstract class BaseCommand extends ContainerAwareCommand
 {
+    /**
+     * @var int
+     */
+    protected $adminUserId;
+
+    /**
+     * @var UserManager|UserService
+     */
+    protected $userManager;
+
+
+    protected function initialize(InputInterface $input, OutputInterface $output)
+    {
+        parent::initialize($input, $output);
+
+        $container = $this->getContainer();
+        $this->adminUserId = (int)$container->getParameter('origammi.ez_app.manager.admin_user.id');
+        $this->userManager = $container->get('origammi.ez_app.manager.user');
+    }
+
 
     /**
      * @return UserManager|UserService
      */
     protected function getUserManager()
     {
-        return $this->getContainer()->get('origammi_ezapp.manager.user');
+        return $this->userManager;
     }
 
+
     /**
-     * @return ConfigResolverInterface
+     * Returns value for $paramName, in $namespace.
+     *
+     * @param string $paramName The parameter name, without $prefix and the current scope (i.e. siteaccess name).
+     * @param string $namespace Namespace for the parameter name. If null, the default namespace should be used.
+     * @param string $scope     The scope you need $paramName value for.
+     *
+     * @throws \eZ\Publish\Core\MVC\Exception\ParameterNotFoundException
+     *
+     * @return mixed
      */
-    protected function getConfigResolver()
+    protected function getScopeParameter($paramName, $namespace = null, $scope = null)
     {
-        return $this->getContainer()->get('ezpublish.config.resolver');
+        return $this->getContainer()->get('ezpublish.config.resolver')->getParameter($paramName, $namespace, $scope);
     }
+
 }
